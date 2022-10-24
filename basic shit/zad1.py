d24baad6afa19fed1f5ce77eecd0c009aaaeeb3b
mirchevski <116561808+mirchevski@users.noreply.github.com>
2022-10-24 15:05:05 +0300

    napravi 2ra zad (ez clap)

--- basic shit/zad1.py
+++ basic shit/zad1.py
@@ -1,1 +1,10 @@
-print(hello)
+date = input("Enter currnet date: ")
+month = input("Enter month: ")
+if (date.endswith("1")):
+    print("Today's date is " + date + "st of " + month)
+elif (date.endswith("2")):
+    print("Today's date is " + date + "nd of " + month)
+elif (date.endswith("3")):
+    print("Today's date is " + date + "rd of " + month)
+else:
+    print("Today's date is " + date + "th and the month is " + month)
---
+++ basic shit/zad2.py
@@ -0,0 +1,11 @@
+from tkinter import E
+
+
+a=60200000
+b=0.008750
+c=1.23e-5
+
+print('a=%.2e'%a)
+print('a=%.2e'%a)
+print('c=%f'%c)
+
