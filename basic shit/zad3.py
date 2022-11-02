3553e01a5e5200b7cef0b06409f85ce888f18bab
mirchevski <116561808+mirchevski@users.noreply.github.com>
2022-11-02 14:28:32 +0200

    1va zad

---
+++ basic shit/zad3.py
@@ -0,0 +1,9 @@
+from cgi import print_form
+
+
+radiant=int(input("How much rad: "))
+degress = radiant*57.295
+
+
+print()
+
---
+++ basic shit/zad4.py
@@ -0,0 +1,6 @@
+a=float(input("Side a is: "))
+b=float(input("Side b is: "))
+h=float(input("Height is: "))
+
+surface = (a+b)*h/2
+print("%.2f" % surface)
